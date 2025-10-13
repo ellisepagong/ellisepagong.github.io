8f57b2ce2f33e7bd6db3b4e85a74e62544c9774f
ellisepagong <ellise.pagong@gmail.com>
2025-10-13 16:43:40 +0800

    wokring tab switch animation

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 console.log("script loaded");
-var profSection = document.getElementById("professional-section");
-var acadSection = document.getElementById("academic-section");
+var profSection = document.getElementById('professional-section');
+var acadSection = document.getElementById('academic-section');
 
-acadSection.classList.add("hide");
+acadSection.classList.add('hidden');
 
-document.getElementById("professional-button").addEventListener("click", ()=>{
-        profSection.classList.remove("hide");
-        acadSection.classList.add("hide");
-
+document.getElementById('professional-button').addEventListener("click", ()=>{
+    if(profSection.classList.contains('hidden')){
+        acadSection.classList.add('hidden')
+        setTimeout(() => {
+            profSection.classList.remove('hidden');
+        }, 200); //
+    }
 });
 
-document.getElementById("academic-button").addEventListener("click", ()=>{
-        profSection.classList.add("hide");
-        acadSection.classList.remove("hide");
+document.getElementById('academic-button').addEventListener("click", ()=>{
+    if(acadSection.classList.contains('hidden')){
+        profSection.classList.add('hidden')
+        setTimeout(() => {
+            acadSection.classList.remove('hidden');
+        }, 200); //
+    }
 });
-
-
 
