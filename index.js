e3d5fdb41aa6ffc68e4439c218de93e25b02b3fb
ellisepagong <ellise.pagong@gmail.com>
2025-11-13 15:34:54 +0800

    Pre- UI Overhaul

--- index.js
+++ index.js
@@ -2,65 +2,98 @@ console.log("script loaded");
 
 // -- INITIAL STATES & GLOBAL VARIABLES 
 
+const buttons = document.querySelector('.navbar').getElementsByTagName('button');
 var profSection = document.getElementById('professional-section');
 var acadSection = document.getElementById('academic-section');
 
-// get list of sections; go to home section at start
+// get list of sections; set home section at start
 const sections = document.querySelectorAll('.section');
 var currSection = 0;
-sections.item(currSection).scrollIntoView();
+scroll(currSection);
 
 // show professional section first
 acadSection.classList.add('hidden');
 
 
 // -- SCROLL BEHAVIOR (disabled when on mobile)
-let throttleTimeout = null;
-document.addEventListener('wheel', e => {
-    if (!throttleTimeout) {
-    throttleTimeout = setTimeout(function() {
-        const dir = e.deltaY > 0 ? 'next' : 'prev';
-        scroll(dir);
-      throttleTimeout = null; 
-    }, 200); // Execute at most every 0.5s
-  }
-});
+// let throttleTimeout = null;
+// document.addEventListener('wheel', e => {
+//     if (!throttleTimeout) {
+//         const dir = e.deltaY > 0 ? 'next' : 'prev';
+//         scroll(dir);
+//         throttleTimeout = setTimeout(function() {
+//             throttleTimeout = null; 
+//         }, 500); // Execute at most every 0.5s
+//   }
+// });
 
 // -- NAVIGATION BEHAVIOR
+document.querySelector('.navbar').addEventListener("click", function (event) {
+    for (let i in buttons){
+        console.log(buttons[i]);
+        if (event.target == buttons[i]) {
+            scroll(i);
+        }
+    }
+    
+});
+
 function scroll(i){
     if(i=='next'){
         currSection +=1;
         if(sections.item(currSection) == null){
             currSection = 4;
-        }else{
-            sections.item(currSection).scrollIntoView();
+            return;
         }
     }else if(i=='prev'){
         currSection -=1;
         if(sections.item(currSection) == null){
             currSection = 0;
+            return;
+        }
+    }
+    else{
+        currSection = i;
+    }
+    updateNavbar(currSection);
+    sections.item(currSection).scrollIntoView();
+}
+
+function updateNavbar(section){
+    for (let j =0; j<5; j++){
+        var button = buttons[j]
+        if(currSection == j){
+            button.classList.add('active');
         }else{
-            sections.item(currSection).scrollIntoView();
+            button.classList = '';
         }
     }
 }
 
 
 // -- EXPERIENCE SECTION BEHAVIOR
-document.getElementById('professional-button').addEventListener("click", ()=>{
+
+var profButton = document.getElementById('professional-button');
+var acadButton = document.getElementById('academic-button');
+
+profButton.addEventListener("click", ()=>{
     if(profSection.classList.contains('hidden')){
         acadSection.classList.add('hidden')
         setTimeout(() => {
             profSection.classList.remove('hidden');
+            profButton.classList.add('active');
+            acadButton.classList = '';
         }, 200); //
     }
 });
 
-document.getElementById('academic-button').addEventListener("click", ()=>{
+acadButton.addEventListener("click", ()=>{
     if(acadSection.classList.contains('hidden')){
         profSection.classList.add('hidden')
         setTimeout(() => {
             acadSection.classList.remove('hidden');
+            acadButton.classList.add('active');
+            profButton.classList = '';
         }, 200); //
     }
 });
